44b149ed1feb3dcd464629eb1710a878a4435672
mitaksim1 <miriamt.simonnet@gmail.com>
2021-06-25 15:12:22 +0200

    Fechamento do modal ao clique do botao Cancelar ok

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,4 +1,8 @@
 export default function Modal() {
+
+    const cancelButton = document.querySelector('.button.cancel');
+    cancelButton.addEventListener("click", close);
+
     function open() {
         // Atribuira a classe active à modal
         document.querySelector('.modal-wrapper').classList.add("active");
@@ -6,6 +10,7 @@ export default function Modal() {
 
     function close() {
         // Removera a classe active da modal
+        document.querySelector('.modal-wrapper').classList.remove("active");
     }
 
     return {
