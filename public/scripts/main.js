c3cda43de1650e066683d5c91d1023e004685a87
mitaksim1 <miriamt.simonnet@gmail.com>
2021-06-25 15:46:30 +0200

    Criaçao da funçao handleClick que se ocupa de chamar o método open()

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,23 +2,28 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+const modalTitle = document.querySelector('.modal h2');
+
+const modalDescription = document.querySelector('.modal p');
+
+const modalButton = document.querySelector('.modal button');
+
 // Recuperar todos os botoes com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check');
 
 checkButtons.forEach(button => {
     // Adcionar o eventListener em cada botao
-    button.addEventListener("click", event => {
-        modal.open();
-    });
+    button.addEventListener("click", handleClick);
 });
 
 // Abertura da modal ao clique no botao "Excluir"
 const deleteButton = document.querySelectorAll(".actions a.delete");
 deleteButton.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open();
-    })
-})
+    button.addEventListener("click", handleClick);
+});
 
-// Se o "Marcar como lida" for clicado
+function handleClick(event) {
+    // Abrir modal
+    modal.open();
+}
 
