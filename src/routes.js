6440c561fbd66ec1f9989cf2929fdb790c405021
mitaksim1 <miriamt.simonnet@gmail.com>
2021-06-25 17:19:13 +0200

    Rota criada como exemplo

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const routes = express.Router();
 routes.get('/', (req, res) => res.render("index"));
 routes.get('/room', (req, res) => res.render("room"));
 routes.get('/create-pass', (req, res) => res.render("create-pass"));
+routes.get('/room/:room/:question/:action', (req, res) => res.render("exemplo", {req}));
 
 // Exportamos o arquivo pra que as rotas sejam acessiveis
 module.exports = routes;
