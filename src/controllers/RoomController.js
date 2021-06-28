c526607e0c525f5fec8d14202c124c326e3bd020
mitaksim1 <miriamt.simonnet@gmail.com>
2021-06-28 15:27:14 +0200

    Criando condiçao pra verificar que o id criado nao existe na base antes de criar uma nova sala

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,23 +5,35 @@ module.exports = {
         const db = await Database();
         // Recuperamos a senha digitada no formulario
         const pass = req.body.password;
-
         let roomId= '';
+        let isRoom = true;
 
-        for (let i = 0; i < 6; i++) {
-            roomId += Math.floor(Math.random() * 10).toString();
+        while(isRoom) {
+             // Gera o numero da sala
+            for (let i = 0; i < 6; i++) {
+                roomId += Math.floor(Math.random() * 10).toString();
+            }
+
+            // Verifica se o numero randômico criado ja existe
+            const roomsExistsIds = await db.all(`SELECT id FROM rooms`);
+
+            // Método some() retorna true se condiçao verdadeira sem fazer o loop em todo o vetor
+            isRoom = roomsExistsIds.some(roomExistsId => roomExistsId === roomId);
+
+            if (!isRoom) {
+                /**
+                 * Insere a sala no banco de dados
+                 */
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`);
+            }
         }
-
-        // console.log(parseInt(roomId));
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`);
-
+       
         await db.close();
 
         res.redirect(`/room/${roomId}`);
